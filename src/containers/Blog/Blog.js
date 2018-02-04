e9de846a7d8bcd9baf0389a7511f42123f71caef
Reggie Riser <reggie.riser@janusresearch.com>
2018-02-03 21:04:36 -0500

    Handle 404 situation.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 // import axios from 'axios';
-import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -38,7 +38,8 @@ class Blog extends Component {
                 <Switch>
                     {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
-                    <Redirect from="/" to="/posts"/>
+                    <Route render={() => <h1>Not found</h1>}/>
+                    {/*<Redirect from="/" to="/posts"/>*/}
                     {/*<Route path="/" component={Posts}/>*/}
                 </Switch>
             </div>
